17134df6c45f8090d5186cbb10e68a9ca7c19621
TahaDidIt <taha.majid4487@gmail.com>
2024-12-09 19:05:05 +0000

    Created main menu and sorted out imports

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime
 import gradio as gr
 from matplotlib import pyplot as plt
+import seaborn as sns
+
 
 """
 PLANNED LAYOUT:
@@ -10,4 +13,49 @@ PLANNED LAYOUT:
 3. Main analysis function (and gradio version)
 4. Main menu
 """
+
+
+
+
+
+### MAIN MENU
+
+def main():
+    menu_choice = ""
+
+    while menu_choice != "0":
+            #Spacer for ease of reading
+            print("")
+            print("")
+            print("")
+            print("########## ", "Current Time: ", datetime.now().strftime("%H:%M:%S"))
+            print("")
+            
+            #Menu choices
+            print("Main Menu:")
+            print("0: Exit")
+            print("1: Dominance Analysis")
+            print("2: Launch Gradio web UI")
+             
+            menu_choice = input("Please select option number: ")
+            
+            if menu_choice == "0":
+                 pass
+            elif menu_choice == "1":
+                #dominance_analysis()
+                pass
+            elif menu_choice =="2":
+                #instantiate gradio here
+                pass
+            else:
+                print("Invalid choice, please try again.")
+
+
+
+
+
+### RUN SCRIPT
+if __name__ == "__main__":
+    main()
+    # Or directly launch analysis/gradio UI here
 
