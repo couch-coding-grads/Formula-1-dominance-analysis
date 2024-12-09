ba6ec431c40273600beac1ea4920610244f9adb8
TahaDidIt <taha.majid4487@gmail.com>
2024-12-09 18:21:01 +0000

    First commit - initialised repo

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from datetime import datetime
+import gradio as gr
+from matplotlib import pyplot as plt
+
+"""
+PLANNED LAYOUT:
+1. Set-up
+2. Subfunctions
+3. Main analysis function (and gradio version)
+4. Main menu
+"""
+
