cd0a0377daa5c91bfc8f9eff192d13797b86a661
pd1304 <116435622+pd1304@users.noreply.github.com>
2024-12-10 17:07:10 +0000

    Update season_round count.py
    Code that takes input for a time range (in years), and outputs how many races took place in each of these years.

--- season_round count.py
+++ season_round count.py
@@ -15,17 +15,19 @@ races = pd.read_csv(r'C:\\Documents\\PYTHON COURSE\\Python F1\\f1db_csv\\races.c
 
 
 def get_races():
+
+    # input the upper and lower ranges for the time period 
     get_lower = int(input('Please insert the year of races you would like from: ')) # lower limit of range
     get_upper = int(input('Please insert the year of races you would like upto: ')) # upper limit of range
     
     year_races = pd.DataFrame()
-    #race_count = []
     # this is an enpty dataframe
     
     total_range = list(range(get_lower, get_upper+1))
     for i in total_range:
         year_races = pd.concat([year_races, races.loc[(races.year == i),['raceId', 'year', 'round','name']]])
-          
+        # concatinates all the data from year year into one dataframe          
+    
     race_count = year_races.groupby('year').size()
     # .groupby('x'): groups the dataframe with similar properties for 'X'. 
     #.size(): Tells you the count of the groups 
@@ -33,7 +35,7 @@ def get_races():
     
     print(race_count)
     
-print(get_races())
+get_races()
 
 
 #print('The number of rounds that have taken place this year are {}'.format(race_count))
