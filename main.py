1a0987dc5bd6fd08974144b982edd32ee406a5fd
TahaDidIt <taha.majid4487@gmail.com>
2024-12-13 17:36:31 +0000

    started dominance analysis main function, input val and data query specifically

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import seaborn as sns
 PLANNED LAYOUT:
 1. Set-up
     - data directory filepaths
-    - Global vars or data
+    - Global data that needs tp ne shared across terminal and web UI versions
 2. Subfunctions
 3. Main analysis function (and gradio version)
 4. Main menu & run script
@@ -19,9 +19,15 @@ PLANNED LAYOUT:
 
 ### SET-UP
 
-#Get directory path to use for dataset file paths
+# Get directory path to use for dataset file paths
 base_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(base_dir, "data")
+
+# Load data
+races = pd.read_csv(os.path.join(data_dir, "races.csv"))
+const = pd.read_csv(os.path.join(data_dir, "constructors.csv"))
+const_results = pd.read_csv(os.path.join(data_dir, "constructor_results.csv"))
+const_standings = pd.read_csv(os.path.join(data_dir, "constructor_standings.csv"))
 
 
 
@@ -29,13 +35,69 @@ data_dir = os.path.join(base_dir, "data")
 
 ### Sub functions
 
+def input_validation(name, start_year, end_year):
+    is_valid = False
 
+    # Make sure name is capitalised first letter only
+    # Check if team constructor name is correct, if years are in range and right way around
+
+    #For now, just pass true
+    is_valid = True
+     
+    return is_valid
+
+
+def team_query():
+    
+    pass
 
 
 
 ### Analysis function
 def dominance_analysis():
-     pass
+     
+    # Get input name, year range, and validate data
+    is_valid = False
+    while is_valid != True:
+        name = input("Please enter constructor name: ")
+        start_year = input("Please enter starting year for analysis range: ")
+        end_year = input("Please enter ending year of analysis range: ")
+
+        is_valid = input_validation(name, start_year, end_year)
+    
+
+
+    # Get constructorID
+    constId = const.loc[(const.name == name), ["constructorId"]]
+    #We just want the value, not a dataframe object
+    constId = constId.values[0][0]
+    print("{0} has constructor ID: {1}".format(name, constId))
+
+
+
+    # Query data
+    temp_dataframe = team_query(constId, start_year, end_year)
+
+
+
+
+    print("This is a placeholder line for a breakpoint")
+
+
+
+
+
+
+    #Points calculation
+
+
+    #Season Round and Time mapping
+
+
+    #Visualisation
+
+
+    pass
 
 
 
@@ -64,7 +126,7 @@ def main():
             if menu_choice == "0":
                  pass
             elif menu_choice == "1":
-                #dominance_analysis()
+                dominance_analysis()
                 pass
             elif menu_choice =="2":
                 #instantiate gradio here
@@ -80,4 +142,5 @@ def main():
 if __name__ == "__main__":
     main()
     # Or directly launch analysis/gradio UI here
+    dominance_analysis()
 
